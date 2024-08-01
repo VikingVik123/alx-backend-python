7b2c808f6f8b9a2bea6fb8749371ef2fcf9e1d97
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-01 15:54:39 +0100

    Added task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function to combine 2 seperate strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat function
+    str1: 1st string
+    str2: 2nd string
+    """
+    return (str1 + str2)
