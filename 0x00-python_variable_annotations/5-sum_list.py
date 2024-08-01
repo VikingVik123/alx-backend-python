0a44dd8ae821d31e9ba60569e1df2a29b278f3ca
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-01 17:01:39 +0100

    Added task 4&5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function 2 return sum of values in a list
+"""
+import math
+
+
+def sum_list(input_list: list) -> float:
+    """
+    input_list: list to sum and return the values of
+    """
+    return(math.fsum(input_list))
+
