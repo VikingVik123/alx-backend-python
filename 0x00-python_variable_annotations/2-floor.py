3f9a727efa0889ee2a50f41690d17d666320c946
VikingVik123 <114689303+VikingVik123@users.noreply.github.com>
2024-08-01 16:10:33 +0100

    Added task 2&3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function to return d floor of a float
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    n: float value which has its floor displayed
+    """
+    return (math.floor(n))
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Func 2 return the str representation of a float
+"""
+
+
+def to_str(n: float) -> float:
+    """
+    n: float to be converted to string
+    """
+    return (str(n))
